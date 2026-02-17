bcb7a356d1eb47bfaa01b163103baa0b84838b7e
Alexey Kulakov <uni_que@me.com>
2026-02-17 05:47:13 -0800

    ui: refactor image-file to use @action wrapper instead of init+bind

--- ui/app/components/image-file.js
+++ ui/app/components/image-file.js
@@ -27,11 +27,6 @@ export default class ImageFile extends Component {
   width = 0;
   height = 0;
 
-  init() {
-    super.init(...arguments);
-    this.updateImageMeta = this.updateImageMeta.bind(this);
-  }
-
   @computed('src')
   get fileName() {
     if (!this.src) return undefined;
